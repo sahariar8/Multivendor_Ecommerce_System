a778da2c28776e7ea1d2b967711f01e52074df5b
sahariar8 <sahariaralam8@gmail.com>
2025-04-16 05:14:16 +0600

    User dashboard mastering and reelated all task

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\UserDashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[HomeController::class,'index'])->name('home');
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -32,3 +31,8 @@ require __DIR__.'/auth.php';
 
 // Admin Routes
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
+//user Dashboard routes
+Route::group(['middleware'=>['auth','verified'],'prefix'=>'user', 'as'=>'user.'],function(){
+    Route::get('dashboard', [UserDashboardController::class, 'UserDashboard'])->name('dashboard');
+});
