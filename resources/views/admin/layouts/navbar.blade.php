4eec9f5e655313d4529bf0e91f740db541cbbd74
sahariar8 <sahariaralam8@gmail.com>
2025-04-15 18:47:38 +0600

    admin login page mastering and logout

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -194,8 +194,11 @@
         </div>
       </li>
       <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-        <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-        <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div></a>
+        <img alt="image" src="{{ asset('backend/assets/img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
+        <div class="d-sm-none d-lg-inline-block">Hi,
+            @auth
+                {{ Auth::user()->name }}
+            @endauth</div></a>
         <div class="dropdown-menu dropdown-menu-right">
           <div class="dropdown-title">Logged in 5 min ago</div>
           <a href="features-profile.html" class="dropdown-item has-icon">
@@ -208,9 +211,16 @@
             <i class="fas fa-cog"></i> Settings
           </a>
           <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item has-icon text-danger">
-            <i class="fas fa-sign-out-alt"></i> Logout
-          </a>
+
+          <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <a href="{{ route('logout') }}"  
+                    onclick="event.preventDefault();this.closest('form').submit();" 
+                    class="dropdown-item has-icon text-danger">
+                <i  class="fas fa-sign-out-alt"></i> Logout
+            </a>
+        </form>
+          
         </div>
       </li>
     </ul>
