3b48ce57a13243759722fa2db07ec39776bd26b6
sahariar8 <sahariaralam8@gmail.com>
2025-04-15 15:07:08 +0600

    create routes for admin & vendor

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +31,11 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+/*   Admin Routes   */
+Route::get('/admin/dashboard',[AdminController::class,'index'])->name('admin.dashboard')->middleware('auth');
+
+/*   Vendor Routes   */
+Route::get('/vendor/dashboard',[VendorController::class,'index'])->name('vendor.dashboard')->middleware('auth');
+ 
